bf62589ad16efc24bfec823d2602f633ba0e4fc5
Alex Zhuravlev <leftemail17@mail.ru>
2025-03-03 16:17:58 +0300

    add product demo img

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import ButtonLogin from "@/components/ButtonLogin";
 import ListItem from "@/components/ListItem";
 import FAQListItem from "@/components/FAQListItem";
+import Image from "next/image";
+import productDemo from "@/public/assets/productDemo.jpeg";
 
 export default function Home() {
   const isLoggedIn = true;
@@ -16,7 +18,7 @@ export default function Home() {
     <main>
       {/* Header */}
       <section className="bg-base-300">
-        <div className="flex justify-between items-center px-8 py-2 mx-auto max-w-3xl">
+        <div className="flex justify-between items-center px-8 py-2 mx-auto max-w-5xl">
           <div className="font-bold">CodeFastSaas</div>
           <div className="space-x-4 max-md:hidden">
             <a href="#pricing" className="link link-hover">
@@ -33,15 +35,23 @@ export default function Home() {
       </section>
 
       {/* Hero */}
-      <section className="text-center py-32 px-8 mx-auto max-w-3xl">
-        <h1 className="text-4xl font-extrabold mb-6 lg:text-5xl">
-          Collect customer feedback to build better products
-        </h1>
-        <div className="opacity-90 mb-10">
-          Create a feedback board in minutes, prioritize features, and build
-          products your customers will love.
+      <section className="flex flex-col gap-14 text-center py-32 px-8 mx-auto max-w-5xl items-center md:items-start md:text-left md:flex-row">
+        <Image
+          src={productDemo}
+          alt="Product demo"
+          className="w-96 rounded-xl"
+        />
+
+        <div>
+          <h1 className="text-4xl font-extrabold mb-6 lg:text-5xl">
+            Collect customer feedback to build better products
+          </h1>
+          <div className="opacity-90 mb-10">
+            Create a feedback board in minutes, prioritize features, and build
+            products your customers will love.
+          </div>
+          <ButtonLogin isLoggedIn={isLoggedIn} />
         </div>
-        <ButtonLogin isLoggedIn={isLoggedIn} />
       </section>
 
       {/* Pricing */}
