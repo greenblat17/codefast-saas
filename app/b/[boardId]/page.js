1cc69edbdc9031ad6f26b16521df08fd0c787a7d
Alex Zhuravlev <leftemail17@mail.ru>
2025-06-06 20:49:35 +0300

    feat: enhance public feedback board with post listing and form integration

--- app/b/[boardId]/page.js
+++ app/b/[boardId]/page.js
@@ -2,28 +2,51 @@ import connectMongo from "@/libs/mongoose";
 import Board from "@/models/Board";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
-const getBoard = async (boardId) => {
+import FormAddPost from "@/components/FormAddPost";
+import Post from "@/models/Post";
+import CardPost from "@/components/CardPost";
+
+const getData = async (boardId) => {
   const session = await auth();
 
   await connectMongo();
 
   const board = await Board.findById(boardId);
+  const posts = await Post.find({ boardId }).sort({ createdAt: -1 });
 
   if (!board) {
     redirect("/");
   }
 
-  return board;
+  return {
+    board,
+    posts,
+  };
 };
 
 export default async function PublicFeedbackBoard(props) {
   const { boardId } = props.params;
 
-  const board = await getBoard(boardId);
+  const { board, posts } = await getData(boardId);
 
   if (!board) {
     return <div>Board not found</div>;
   }
 
-  return <main>{board.name} (public)</main>;
+  return (
+    <main className="min-h-screen bg-base-200">
+      <section className="max-w-5xl mx-auto p-5">
+        <h1 className="text-lg font-bold">{board.name}</h1>
+      </section>
+
+      <section className="max-w-5xl mx-auto px-5 flex flex-col md:flex-row md:items-start gap-8 pb-12">
+        <FormAddPost boardId={boardId} />
+        <ul className="space-y-4 flex-grow">
+          {posts.map((post) => (
+            <CardPost key={post._id} post={post} />
+          ))}
+        </ul>
+      </section>
+    </main>
+  );
 }
--- models/Post.js
+++ models/Post.js
@@ -1,28 +1,33 @@
 import mongoose from "mongoose";
 
-const postSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-    maxLength: 100,
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 100,
+    },
+    description: {
+      type: String,
+      trim: true,
+      maxLength: 1000,
+    },
+    boardId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Board",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    trim: true,
-    maxLength: 1000,
-  },
-  boardId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Board",
-    required: true,
-  },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Post = mongoose.models.Post || mongoose.model("Post", postSchema);
 
